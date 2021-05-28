79322d1aa49f94f3386b44f83046f2b57779f228
Sweet-Bird1 <74974926+Sweet-Bird1@users.noreply.github.com>
2021-05-28 19:03:13 +0800

    Add files via upload

---
+++ cnn_model.py
@@ -0,0 +1,75 @@
+# coding: utf-8
+
+import tensorflow as tf
+
+
+class TCNNConfig(object):
+    """CNN配置参数"""
+
+    embedding_dim = 64  # 词向量维度
+    seq_length = 600  # 序列长度
+    num_classes = 14  # 类别数
+    num_filters = 256  # 卷积核数目
+    kernel_size = 5  # 卷积核尺寸
+    vocab_size = 5000  # 词汇表达小
+
+    hidden_dim = 256  # 全连接层神经元
+
+    dropout_keep_prob = 0.5  # dropout保留比例
+    learning_rate = 1e-3  # 学习率
+
+    batch_size = 256  # 每批训练大小
+    num_epochs = 30  # 总迭代轮次
+
+    print_per_batch = 200  # 每多少轮输出一次结果
+    save_per_batch = 10  # 每多少轮存入tensorboard
+
+
+class TextCNN(object):
+    """文本分类，CNN模型"""
+
+    def __init__(self, config):
+        self.config = config
+
+        # 三个待输入的数据（提前分配内存，占位作用）
+        self.input_x = tf.placeholder(tf.int32, [None, self.config.seq_length], name='input_x')#dtype,shape=None,name=None
+        self.input_y = tf.placeholder(tf.float32, [None, self.config.num_classes], name='input_y')
+        self.keep_prob = tf.placeholder(tf.float32, name='keep_prob')
+
+        self.cnn()
+
+    def cnn(self):
+        """CNN模型"""
+        # 词向量映射
+        with tf.device('/gpu:0'):#指定运算的设备
+            embedding = tf.get_variable('embedding', [self.config.vocab_size, self.config.embedding_dim])#创建新的tf对象（二维矩阵）
+            embedding_inputs = tf.nn.embedding_lookup(embedding, self.input_x)#将词id转换成对应的向量
+
+        with tf.name_scope("cnn"):#构建命名空间，确保每个变量的名是独一无二的
+            # CNN layer
+            conv = tf.layers.conv1d(embedding_inputs, self.config.num_filters, self.config.kernel_size, name='conv')
+            # global max pooling layer
+            gmp = tf.reduce_max(conv, reduction_indices=[1], name='gmp')
+
+        with tf.name_scope("score"):
+            # 全连接层，后面接dropout以及relu激活
+            fc = tf.layers.dense(gmp, self.config.hidden_dim, name='fc1')
+            fc = tf.contrib.layers.dropout(fc, self.keep_prob)
+            fc = tf.nn.relu(fc)
+
+            # 分类器
+            self.logits = tf.layers.dense(fc, self.config.num_classes, name='fc2')
+            self.soft = tf.nn.softmax(self.logits)
+            self.y_pred_cls = tf.argmax(self.soft, 1)  # 预测类别
+
+        with tf.name_scope("optimize"):
+            # 损失函数，交叉熵
+            cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=self.logits, labels=self.input_y)
+            self.loss = tf.reduce_mean(cross_entropy)
+            # 优化器
+            self.optim = tf.train.AdamOptimizer(learning_rate=self.config.learning_rate).minimize(self.loss)
+
+        with tf.name_scope("accuracy"):
+            # 准确率
+            correct_pred = tf.equal(tf.argmax(self.input_y, 1), self.y_pred_cls)
+            self.acc = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
